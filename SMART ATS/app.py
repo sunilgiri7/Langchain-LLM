29e7c4b633dcb1c7f60b983f2343a2de5fea242e
sunilgiri7 <seungiri841@gmail.com>
2024-01-20 10:46:00 -0800

    new

--- SMART ATS/app.py
+++ SMART ATS/app.py
@@ -18,22 +18,23 @@ def pdf_to_text(uploadedFile):
     text = ""
     for i in range(len(reader.pages)):
         page = reader.pages[i]
-        text += str(page.extract_text)
+        text += page.extract_text()
     return text
 
+
 input_prompt = """
-    Evaluate the candidate's resume for a position in the technology field, focusing on software engineering, data science, data analysis, big data engineering, and Python development. 
-    As a seasoned Application Tracking System (ATS), your goal is to provide a detailed analysis considering the competitive job market. 
-    Assign a percentage match based on the given job description and identify missing keywords with high accuracy.
+Evaluate the candidate's resume for a position in the technology field, focusing on software engineering, data science, data analysis, data engineering, big data engineering, and Python development. 
+As a seasoned Application Tracking System (ATS), your goal is to provide a detailed analysis considering the competitive job market. 
+Assign a percentage match based on the given job description and identify missing keywords with high accuracy.
 
-    Resume:
-    {text}
+Resume:
+{text}
 
-    Job Description:
-    {jd}
+Job Description:
+{jd}
 
-    Provide a response in the following format:
-    {"JD Match": "%", "Missing Keywords": [], "Profile Summary": ""}
+Provide a response in the following format:
+{{"JD Match": "%", "Missing Keywords": [], "Profile Summary": ""}}
 """
 
 
@@ -43,8 +44,10 @@ st.text("Improve Your Resume By Using Most Advanced ATS System")
 jd = st.text_area("Paste the Job Description")
 uploaded_file = st.file_uploader("Upload Your Resume", type="pdf", help="please upload the PDF")
 submit = st.button("Submit")
+
 if submit:
     if uploaded_file is not None:
         text = pdf_to_text(uploaded_file)
-        response = get_gemini_response(input_prompt)
+        response = get_gemini_response(input_prompt.format(text=text, jd=jd))
         st.subheader(response)
+
